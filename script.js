ac03adc0d718ac56cd33b85d3c11e74c01ddcecc
Ankush Wadhwani <ankushwadhwani222@gmail.com>
2024-09-15 01:01:54 +0530

    Retained progress using localStorage

--- script.js
+++ script.js
@@ -1,36 +1,35 @@
 let prog = 0;
 const checkboxes = document.querySelectorAll('.checkbox-input');
+const inputTexts = document.querySelectorAll('.text-input');
 const inputElements = document.querySelector('#input-elements');
 const progress = document.querySelector('.progress');
 const progressText = document.querySelector('#progress-text');
 const progressFullText = document.querySelector('#progress-full-text');
 const noTaskError = document.querySelector('#no-task-error');
 const allGoalsError = document.querySelector('#all-goals-error');
-inputElements.addEventListener('click', () => {
-    prog = 0;
-    let hasInput = true;
-    checkboxes.forEach((checkbox, index) => {
-        const inputText = document.querySelector(`#text-input-${index + 1}`);
-        if (checkbox.checked && !inputText.value) {
-            noTaskError.classList.remove('hidden');
-        }
-        else if (checkbox.checked && inputText.value) {
-            noTaskError.classList.add('hidden');
-            prog++;
-        }
-        else if(!checkbox.checked && inputText.value){
-            noTaskError.classList.add('hidden');
-            
-        }
-        if (!inputText.value) {
-            hasInput = false;
-        }
-    });
-    if (hasInput === true) {
-        allGoalsError.classList.add('hidden');
+inputElements.addEventListener('click', main);
+function main(){
+  prog = 0;
+  let hasInput = true;
+  checkboxes.forEach((checkbox, index) => {
+    const inputText = document.querySelector(`#text-input-${index + 1}`);
+    if (checkbox.checked && !inputText.value) {
+      noTaskError.classList.remove("hidden");
+    } else if (checkbox.checked && inputText.value) {
+      noTaskError.classList.add("hidden");
+      prog++;
+    } else if (!checkbox.checked && inputText.value) {
+      noTaskError.classList.add("hidden");
     }
-    updateProgress(prog);
-})
+    if (!inputText.value) {
+      hasInput = false;
+    }
+  });
+  if (hasInput === true) {
+    allGoalsError.classList.add("hidden");
+  }
+  updateProgress(prog);
+}
 function isChecked(checkbox) {
     if (checkbox.checked) {
         return true;
@@ -49,3 +48,37 @@ function updateProgress(prog) {
         progressFullText.classList.remove('hidden');
     }
 }
+let savedData = {
+    inputs: [],
+    checkboxes:[]
+}
+function saveData() {
+    savedData.checkboxes = [];
+    savedData.inputs = [];
+    checkboxes.forEach((checkbox, index) => {
+        const inputText = document.querySelector(`#text-input-${index + 1}`);
+        savedData.inputs.push(inputText.value);
+        savedData.checkboxes.push(checkbox.checked);
+    })
+    localStorage.setItem('savedData', JSON.stringify(savedData));
+}
+checkboxes.forEach((checkbox) => {
+    checkbox.addEventListener('change', saveData);
+})
+inputTexts.forEach((inputText) => {
+    inputText.addEventListener('input', saveData);
+})
+function loadData() {
+    let loadedData = localStorage.getItem('savedData');
+    if (loadedData) {
+        loadedData = JSON.parse(loadedData);
+        checkboxes.forEach((checkbox, index) => {
+            const inputText = document.querySelector(`#text-input-${index + 1}`);
+            checkbox.checked = loadedData.checkboxes[index];
+            inputText.value = loadedData.inputs[index];
+        })
+    }
+    main();
+}
+
+window.addEventListener('load', loadData);
